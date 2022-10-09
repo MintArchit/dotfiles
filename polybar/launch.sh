02766ddf268635bdfc42dbb9ed66c013b9dc17be
mintarchit <mintarchit@gmail.com>
2022-10-09 12:27:41 +0200

    fix font

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -12,8 +12,11 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "---" | tee -a /tmp/polybar1.log
+polybar -c=$HOME/.config/polybar/config.ini top 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
